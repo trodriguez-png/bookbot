bf9c7daa9f6c234889b2bfb4195a461325e3e5df
trodriguez-png <trodriguez@datservicesinc.com>
2025-11-27 20:17:08 -0500

    BookBot project

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import sys
+print(sys.argv)
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print ("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     dictionary = word_appearance(text)
